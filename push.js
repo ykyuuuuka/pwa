0e478285791d84bad5a40e177d2c0068499368b2
皆銭 祐哉 <y-kaisen@ad.imjp.co.jp>
2017-12-27 14:12:58 +0900

    add files

--- push.js
+++ push.js
@@ -6,9 +6,9 @@ const GCM_API_KEY = 'AIzaSyByQsBd9GRa9kxR6NWPYQRzHc-Jfw0HuAs';
 push.setGCMAPIKey(GCM_API_KEY);
 
 const data = {
-	'endpoint': '**エンドポイントを指定**',
-	'userAuth': '**鍵生成に使用する乱数指定**',
-	'userPublicKey': '**公開鍵指定**'
+	'endpoint': 'https://android.googleapis.com/gcm/send/eDHsFIEiur4:APA91bEkutRTkRTErxGJNzmafa3ZM9NS5JEwpV2DnFu3kAlPIZZApAnBYt-L0Pe4O_KU_8ym-_bguKyD5bXirL0W9aZMfUg7ii9KzUdFjmnTMrkJo942HhLUSJwG_Avl23IILJoyZj7B',
+	'userAuth': '3aAUCzs/azZDndQANGhjag==',
+	'userPublicKey': 'BNDAvDlXTimNHp3RaPXjnt1wocrPtwZGwpNi0JIi/tW0s1tXcvTsv/SgixKNar4oowa8TYB/MQEc5QlQcXHi6Tc='
 };
 
 const pushSubscription = {
