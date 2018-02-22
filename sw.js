9eb0de8c56553013995371a0f41d120716ddbbbb
MARS\y-kaisen <y-kaisen@imjp.co.jp>
2018-02-22 20:33:57 +0900

    commit

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-console.log('sw.jsのスクリプトが実行されたはず');
+console.log('sw.jsのスクリプトが実行されていればこのメッセージが表示される');
 
 self.addEventListener('install', function(event) {
-	console.log('V2 installing…');
+	console.log('V3 installing…');
 
 	// skipWaiting() を使えば強制的にswを更新できる
 	event.waitUntil(self.skipWaiting());
@@ -9,7 +9,7 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('activate', function(event) {
-	console.log('V2 activating…');
+	console.log('V3 activating…');
 
 	// 強制的にclaim() を実行
 	event.waitUntil(self.clients.claim());
@@ -17,5 +17,5 @@ self.addEventListener('activate', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-	console.log('V2 fetching…');
+	console.log('V3 fetching…');
 });
