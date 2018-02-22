733a2c61d6efa03aa2221fa009fd88dc0ba34d70
MARS\y-kaisen <y-kaisen@imjp.co.jp>
2018-02-22 20:22:35 +0900

    commit

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
 console.log('sw.jsのスクリプトが実行されたはず');
+
+self.addEventListener('install', function(event) {
+	console.log('V1 installing…');
+}
+
+self.addEventListener('activate', function(event) {
+	console.log('V1 activating…');
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log('V1 fetching…');
+});
